a34f5de18f0fbe9000f73fd3f45759f8899d42b2
Maddi8 <61945736+Maddi8@users.noreply.github.com>
2023-12-24 00:34:52 +0200

    Update App.tsx

--- osa1/kurssitiedot/src/App.tsx
+++ osa1/kurssitiedot/src/App.tsx
@@ -6,27 +6,21 @@ const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
-      {
-        part: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        part: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        part: 'State of a component',
-        exercises: 14
-      }
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
     ]
-  }
+  };
+
+  const totalExercises = course.parts.reduce((total, part) => total + part.exercises, 0);
+
   return (
     <div>
       <Header headerText={course.name} />
-      <Content parts={course.parts.map((obj) => obj.part)} exercises={course.parts.map((obj) => obj.exercises)} />
-      <Total total={course.parts.map((obj) => obj.exercises).reduce((a, b) => a + b, 0)} />
+      <Content parts={course.parts.map(part => part.name)} exercises={course.parts.map(part => part.exercises)} />
+      <Total total={totalExercises} />
     </div>
-  )
-}
+  );
+};
 
 export default App
